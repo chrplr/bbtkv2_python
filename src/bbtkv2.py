560d751bc36214f6b10f702057190190b1275f82
Christophe Pallier <christophe@pallier.org>
2022-04-18 13:31:48 +0200

    preparation for pip

--- src/bbtkv2.py
+++ src/bbtkv2.py
@@ -23,50 +23,86 @@ import serial
 import time
 import sys
 import pandas as pd
+import toml
 
-DEBUG = True
-CRLF = '\r\n'
-WT = 0.100  # waiting time between writing commands (100 ms)
+##############################################################################
 
-sensor_thresholds = {'Mic1': 63,
-                     'Mic2': 63,
-                     'Sounder1': 63,
-                     'Sounder2': 63,
-                     'Opto1': 63,
-                     'Opto2': 63,
-                     'Opto3': 63,
-                     'Opto4': 63}
+DEFAULT_CONFIG = """
+serialport="/dev/ttyACM0"
+baudrate=230400
+debug=1
 
+[sensor_thresholds]
+Mic1=0
+Mic2=0
+Sounder1=0
+Sounder2=0
+Opto1=110
+Opto2=110
+Opto3=110
+Opto4=110
+"""
 
-input_port_names = ('Keypad4', 'Keypad3', 'Keypad2', 'Keypad4',
+INPUT_PORT_NAMES = ('Keypad4', 'Keypad3', 'Keypad2', 'Keypad1',
                     'Opto4', 'Opto3', 'Opto2', 'Opto1',
                     'TTLin2', 'TTLin1', 'Mic2', 'Mic1')
 
-output_port_names = ('ActClose4', 'ActClose3', 'ActClose2', 'ActClose1',
+OUTPUT_PORT_NAMES = ('ActClose4', 'ActClose3', 'ActClose2', 'ActClose1',
                      'TTLout2', 'TTLout1', 'Sounder2', 'Sounder1')
+
+DSC_LINE_NAMES = ['timestamp'] + \
+                 list(INPUT_PORT_NAMES) + \
+                 list(OUTPUT_PORT_NAMES)
+
+###############################################################################
+
+CRLF = '\r\n'
+WT = 0.05  # waiting time (50 ms) between writes on serial port 
+
+###############################################################################
 
 
 def inp_port_mask12_to_series(mask12):
     assert type(mask12) is str
     assert len(mask12) == 12
-    return pd.Series([int(b) for b in mask12], index=input_port_names)
+    return pd.Series([int(b) for b in mask12], index=INPUT_PORT_NAMES)
 
 
 def out_port_mask8_to_series(mask8):
     assert type(mask8) is str
     assert len(mask8) == 8
-    return pd.Series([int(b) for b in mask8], index=output_port_names)
+    return pd.Series([int(b) for b in mask8], index=OUTPUT_PORT_NAMES)
 
 
-def dsc_mask32_to_series(mask32):
+def dsc_mask32_to_list(mask32):
     assert type(mask32) is str
     assert len(mask32) == 32
+    # the time-stamp (in microsec) is stored as the last 12 digits
+    time_stamp = int(mask32[20:]) / 1000.0  # convert to msec
+    return [time_stamp] + [int(b) for b in mask32[:20]]
 
-    inputs = mask32[:12]
-    outputs = mask32[12:20]
-    time_stamp = int(mask32[20:]) / 1000.0
-    #TODO: make it a pd.Series!
-    return [time_stamp, inp_port_mask12_to_series(inputs), out_port_mask8_to_series(outputs)]
+
+DSC_OUTPUT_EXAMPLE = """
+SDAT;
+3;
+30000000;
+120000;
+11001100110001010101000000123456;
+01001100110001010101000000234567;
+11001100110001010101000000345678;
+EDAT;
+"""
+
+
+def dsc_output_to_dataframe(text):
+    all_events = []
+    for row in text.split(';'):
+        line = row.strip()
+        if len(line) == 32:
+            all_events.append(dsc_mask32_to_list(line))
+    df = pd.DataFrame(all_events, columns=DSC_LINE_NAMES)
+    return df
+
 
 # a = '11001100110001010101000123456789'
 # bbtkv2.dsc_mask32_to_series(a)
@@ -97,10 +133,22 @@ def dsc_mask32_to_series(mask32):
 
 
 class BlackBoxToolKit:
-    def __init__(self, port='/dev/ttyACM0', baudrate=230400):
-        # TODO: read parameters from ini file
-        self.bbtk = serial.Serial(port=port,
-                                  baudrate=baudrate,
+    def __init__(self, config_file=None, config_string=None):
+        """ Read configuration and opens the serial port associated to the bbtk. """
+        if config_file is not None:
+            self.settings = toml.load(config_file)
+        elif config_string is not None:
+            self.settings = toml.loads(config_string)
+        else:
+            self.settings = toml.loads(DEFAULT_CONFIG)
+
+        self.debug = self.settings['debug'] == 1
+
+        if self.debug:
+            print("** BBTK Settings:\n\n", toml.dumps(self.settings))
+
+        self.bbtk = serial.Serial(port=self.settings['serialport'],
+                                  baudrate=self.settings['baudrate'],
                                   bytesize=serial.EIGHTBITS,
                                   parity=serial.PARITY_NONE,
                                   stopbits=serial.STOPBITS_ONE,
@@ -109,34 +157,44 @@ class BlackBoxToolKit:
                                   rtscts=False,
                                   dsrdtr=False,
                                   writeTimeout=1)
-        self.thresholds = sensor_thresholds
+      
+        self.thresholds = self.settings['sensor_thresholds']
+        self.connect()
+        self.set_sensor_thresholds()
+        
+    def get_current_settings(self):
+        return self.settings
+
+    def read_thresholds_from_toml_file(self, filename):
+        settings = toml.load(filename)
+        self.thresholds = settings['sensor_thresholds']
 
     def soft_reset(self):
+        if (self.debug):
+            print('Sending a soft break to the BBTK')
         self.bbtk.sendBreak()
 
     def send_command(self, cmd):
-        if (DEBUG):
+        if self.debug:
             print('Sending: ' + cmd)
         self.bbtk.write((cmd + CRLF).encode())
         time.sleep(WT)
 
-    def getresponse(self):   # does not stop !!!
-        while (self.bbtk.is_open):
-            # Check if incoming bytes are waiting to be read from the serial input
-            # buffer.
+    def get_response(self, timeout=10):
+        """ accumulates characters from serial channel.
+            Returns the string of characters.
+        """
+        start_time = time.time()
+        data_str = ""
+        while (self.bbtk.is_open and ((time.time() - start_time) < timeout)):
             if (self.bbtk.in_waiting > 0):
-                # read the bytes and convert from binary array to ASCII
-                data_str = self.bbtk.read(self.bbtk.in_waiting).decode('ascii')
-                # print the incoming string without putting a new-line
-                # ('\n') automatically after every print()
-                print(data_str, end='')
+                chars = self.bbtk.read(self.bbtk.in_waiting).decode('ascii')
+                data_str += chars
+                if self.debug:
+                    print(chars, end='')
+            time.sleep(0.05)
 
-            # Put the rest of your code you want here
-
-            # Optional, but recommended: sleep 10 ms (0.01 sec) once per loop to let
-            # other thr
-            time.sleep(0.01)
-
+        return data_str
 
     def read_line(self):
         """ accumulates characters from serial channel until a '\n' is reached.
@@ -144,20 +202,20 @@ class BlackBoxToolKit:
         """
         # TODO: this is currenlty blocking on input :(
         time.sleep(WT)
-        if (DEBUG):
+        if (self.debug):
             print('Waiting for response from BBTK...')
         s = ''
         c = self.bbtk.read().decode('ascii')
-        if (DEBUG):
+        if (self.debug):
             sys.stdout.write(c)
         # while c != ';':
         while c != '\n':
             if c != '\n':
                 s = s + c
             c = self.bbtk.read().decode()
-            if (DEBUG):
+            if (self.debug):
                 sys.stdout.write(c)
-        if (DEBUG):
+        if (self.debug):
             sys.stdout.write('\n')
         return(s)
 
@@ -180,27 +238,27 @@ class BlackBoxToolKit:
         self.send_command('ABOU')
         time.sleep(2)
 
+    def is_alive(self):
+        self.send_command('ECHO')
+        return self.read_line() == 'ECHO'
+        
     def set_smoothing_off(self):
         self.send_command('SMOO')
         self.send_command('00000011')
-        self.read_line()
+        return self.is_alive()
 
     def set_smoothing_on(self):
         self.send_command('SMOO')
         self.send_command('11111111')
-        self.read_line()
-
+        return self.is_alive()
+    
     def set_sensor_thresholds(self, thresholds=None):
         self.send_command("SEPV")
         if thresholds is None:
             thresholds = self.thresholds
         for val in thresholds.values():
             self.send_command(str(val))
-        self.read_line()
-
-    def is_alive(self):
-        self.send_command('ECHO')
-        return self.bbtk.read(4) == 'ECHO'
+        return self.is_alive()
 
     def flush(self):
         self.send_command('FLUS')
@@ -209,15 +267,15 @@ class BlackBoxToolKit:
     def get_sensor_thresholds(self):
         self.send_command('GEPV')
         resp = self.read_line()
-        vals = map(int, resp.split(','))
-        return { 'Mic1': vals[0],
-                 'Mic2': vals[1],
-                 'Sounder1':  vals[2],
-                 'Sounder2': vals[3],
-                 'Opto1': vals[4],
-                 'Opto2': vals[5],
-                 'Opto3': vals[6],
-                 'Opto4': vals[7]}
+        vals = [int(s) for s in resp.split(',')]
+        return {'Mic1': vals[0],
+                'Mic2': vals[1],
+                'Sounder1':  vals[2],
+                'Sounder2': vals[3],
+                'Opto1': vals[4],
+                'Opto2': vals[5],
+                'Opto3': vals[6],
+                'Opto4': vals[7]}
 
     def clear_timing_data(self):
         self.send_command('SPIE')
@@ -231,42 +289,25 @@ class BlackBoxToolKit:
         start_time = time.time()
         while time.time() - start_time < duration:
             resp = self.read_line()
-            print(resp)
         self.soft_reset()
 
-    def digital_stimulus_capture(self, duration=5):
+    def digital_stimulus_capture(self, duration=30):
         self.clear_timing_data()
         self.send_command('DSCM')
         self.send_command('TIML')
         self.send_command(str(duration*1000000))
         self.send_command('RUDS')
-
-        s = self.read_line()
-        if s != 'SDAT':
-            raise Exception('Not receiving data from the BBTK')
-        nevents = int(self.read_line())
-        runtime = int(self.read_line())
-        nsamples = int(self.read_line())
-        lines = []
-        timestamp = []
-        for i in range(nevents):
-            ev = self.read_line()
-            lines.append(ev[:12])
-            timestamp.append(ev[20:])
-        return nevents
-
+        time.sleep(duration)
+        text = self.get_response(5)
+        return dsc_output_to_dataframe(text)
 
 
 def test_acquisition():
     bb = BlackBoxToolKit()
-    bb.connect()
-    bb.display_info_on_bbtk()
-    bb.set_sensor_thresholds()
-    bb.clear_timing_data()
-    nevents = bb.digital_stimulus_capture(5)
-    print("%d events detected" % nevents )
+    events = bb.digital_stimulus_capture(30)
+    print(events)
     bb.disconnect()
 
-
+    
 if __name__ == '__main__':
     test_acquisition()
